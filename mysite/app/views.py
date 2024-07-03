b318b008f05eb653a9eaf7dec1e8963aed803fe7
Mehak <cmehak26@gmail.com>
2024-07-03 06:45:15 +0530

    add some guideline in base.html

--- mysite/app/views.py
+++ mysite/app/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.shortcuts import render
 from django.views import View
+from .models import Product
 
 # Create your views here.
 def home(request):
@@ -9,4 +8,5 @@ def home(request):
 
 class CategoryView(View):
    def get(self,request,val):
+       product = Product.objects.filter()
        return render(request,"app/category.html",locals()) 
